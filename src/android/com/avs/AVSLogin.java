aac4e0a594498f9a27dd4a0e44b45afee99e2e5c
xiangjb <xiangjb@boydev.com>
2017-11-24 11:34:23 +0800

    change android login file

--- src/android/com/avs/AVSLogin.java
+++ src/android/com/avs/AVSLogin.java
@@ -6,6 +6,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
 import org.json.JSONObject;
@@ -24,8 +25,9 @@ import com.amazon.identity.auth.device.api.authorization.Scope;
 import com.amazon.identity.auth.device.api.Listener;
 import com.amazon.identity.auth.device.api.workflow.RequestContext;
 
-public class AVSLogin extends CordovaPlugin
-{
+public class AVSLogin extends CordovaPlugin {
+    private static final String TAG = "AVSLogin";
+
     private static final String ACTION_AUTHORIZE = "authorize";
     private static final String ACTION_GET_TOKEN = "getToken";
     private static final String ACTION_SIGNOUT = "signOut";
@@ -35,36 +37,36 @@ public class AVSLogin extends CordovaPlugin
     private static final String FIELD_USER = "user";
     private static final String FIELD_CLIENT_ID = "clientId";
 
+    private static final String ARG_SCOPE = "scope";
+    private static final String ARG_SCOPE_DATA = "scope_data";
+
+    private static final String Default_Scope = "alexa:all";
+
     private RequestContext mRequestContext;
     private CallbackContext mCallbackContext;
 
     private String CurrScope;
     private JSONObject CurrScopeData;
 
-    public void initialize(final CordovaInterface cordova, CordovaWebView webView)
-    {
+    public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
         mRequestContext = RequestContext.create(cordova.getActivity());
-        requestContext.registerListener(new AuthorizeListener()
-        {
-            
+        mRequestContext.registerListener(new AuthorizeListener() {
+
             @Override
-            public void onSuccess(AuthorizeResult result)
-            {
+            public void onSuccess(AuthorizeResult result) {
                 Log.d(TAG, "Authorization was completed successfully.");
                 /* Your app is now authorized for the requested scopes */
             }
 
             @Override
-            public void onError(AuthError ae)
-            {
+            public void onError(AuthError ae) {
                 Log.e(TAG, "There was an error during the attempt to authorize the application.");
                 /* Inform the user of the error */
                 mCallbackContext.error("Trouble during the attempt to authorize the application");
             }
 
             @Override
-            public void onCancel(AuthCancellation cancellation) 
-            {
+            public void onCancel(AuthCancellation cancellation) {
                 Log.d(TAG, "Authorization was cancelled before it could be completed. ");
                 /* Reset the UI to a ready-to-login state */
                 mCallbackContext.error("Authorization was cancelled before it could be completed.");
@@ -72,70 +74,47 @@ public class AVSLogin extends CordovaPlugin
         });
     }
 
-    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException
-    {
+    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
         Runnable TempRunnable;
-        if (action.equals(ACTION_AUTHORIZE))
-        {
+        if (action.equals(ACTION_AUTHORIZE)) {
             JSONObject Params = args.getJSONObject(0);
-            CurrScope = Params.optString("scope", "alexa:all");
-            CurrScopeData = Params.optJSONObject("scope_data");
-    
-            if (CurrScope.equals("alexa:all") && ! CurrScopeData)
-            {
+            CurrScope = Params.optString(ARG_SCOPE, Default_Scope);
+            CurrScopeData = Params.optJSONObject(ARG_SCOPE_DATA);
+
+            if (CurrScope.equals(Default_Scope) && CurrScopeData == null) {
                 callbackContext.error("no scope data!");
                 return false;
             }
             TempRunnable = Authroize;
-        }
-        else if (action.equals(ACTION_GET_TOKEN))
-        {
+        } else if (action.equals(ACTION_GET_TOKEN)) {
             TempRunnable = GetToken;
-        }
-        else if (action.equals(ACTION_SIGNOUT))
-        {
+        } else if (action.equals(ACTION_SIGNOUT)) {
             TempRunnable = SignOut;
-        }
-        else
-        {
+        } else {
             callbackContext.error("Not supported actions" + action);
             return false;
         }
 
         mCallbackContext = callbackContext;
         cordova.getThreadPool().execute(TempRunnable);
+        return true;
     }
 
     @Override
-    public void onResume(boolean multitasking)
-    {
+    public void onResume(boolean multitasking) {
         super.onResume(multitasking);
-        requestContext.onResume();
+        mRequestContext.onResume();
     }
 
     @Override
-    public void onStart()
-    {
+    public void onStart() {
         super.onStart();
-    }
 
-    private final Runnable Authroize = new Runnable(){
-    
-        @Override
-        public void run() {
-            AuthorizationManager.authorize(new AuthorizeRequest.Builder(requestContext)
-                .addScope(ScopeFactory.scopeNamed(CurrScope, CurrScopeData))
-                .forGrantType(AuthorizeRequest.GrantType.ACCESS_TOKEN)
-                .shouldReturnUserData(false)
-                .build());
-        }
-    };
+        if (CurrScope == null)
+            CurrScope = Default_Scope;
 
-    private final Runnable GetToken = new Runnable(){
-    
-        @Override
-        public void run() {
-            AuthorizationManager.getToken(this, new Scope[] { ScopeFactory.scopeNamed("alexa:all")}, new Listener<AuthorizeResult, AuthError>() {
+        AuthorizationManager.getToken(cordova.getActivity(), new Scope[] { ScopeFactory.scopeNamed(CurrScope) },
+            new Listener<AuthorizeResult, AuthError>() {
                 @Override
                 public void onSuccess(AuthorizeResult result) {
                     if (result.getAccessToken() != null) {
@@ -148,29 +127,48 @@ public class AVSLogin extends CordovaPlugin
                         Log.d(TAG, "getToken fail----->The user is not signed in");
                     }
                 }
-    
+
                 @Override
                 public void onError(AuthError ae) {
                     /* The user is not signed in */
                 }
             });
+    }
+
+    private final Runnable Authroize = new Runnable() {
+
+        @Override
+        public void run() {
+            AuthorizationManager.authorize(new AuthorizeRequest.Builder(mRequestContext)
+                .addScope(ScopeFactory.scopeNamed(CurrScope, CurrScopeData))
+                .forGrantType(AuthorizeRequest.GrantType.ACCESS_TOKEN).shouldReturnUserData(false)
+                .build());
+        }
+    };
+
+    private final Runnable GetToken = new Runnable() {
+
+        @Override
+        public void run() {
+
         }
     };
 
     private final Runnable SignOut = new Runnable() {
-    
+
         @Override
         public void run() {
-            AuthorizationManager.signOut(cordova.getActivity().getApplicationContext(), new Listener<Void, AuthError>() {
-                @Override
-                public void onSuccess(Void response) {
-                }
+            AuthorizationManager.signOut(cordova.getActivity().getApplicationContext(),
+                    new Listener<Void, AuthError>() {
+                        @Override
+                        public void onSuccess(Void response) {
+                        }
 
-                @Override
-                public void onError(AuthError authError) {
-                    Log.e(TAG, "Error clearing authorization state.", authError);
-                }
-            });
+                        @Override
+                        public void onError(AuthError authError) {
+                            Log.e(TAG, "Error clearing authorization state.", authError);
+                        }
+                    });
         }
     };
 
